387c660b3e85885078202959cd6a778cdb657870
timofeya123 <timofeya123@inbox.ru>
2021-02-13 23:44:47 +0600

    static

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,6 +21,7 @@ public class Main {
         myThread2.join(); // запускает выбранный поток до смерти
 
         System.out.println(resource.i);
+        System.out.println(resource.j);
     }
 }
 
@@ -34,11 +35,13 @@ class MyThread extends Thread { // создать поток при помощи
     @Override
     public void run(){
         resource.chengI(); // запускаем в ресурсе метод i++;
+        resource.chengJ(); // запускаем в ресурсе метод i++;
     }
 }
 
 class Resource{
     int i;
+    static int j=0;
 
     public int getI(){
         return i;
@@ -59,4 +62,17 @@ class Resource{
             this.i=i;
         }
     }
+
+    public static void chengJ(){ // никогда не смешивать статичекую и обычную синхронизацию
+        System.out.println(Resource.j);
+        synchronized (Resource.class){ // не даст доступ другим потокам, пока не выполнится, тот который начал работу
+            int j = Resource.j;
+            if (Thread.currentThread().getName().equals("one")){ // если поток one
+                Thread.yield(); // дать возможность запустится другому потоку
+            }
+            j++;
+            Resource.j= j;
+        }
+    }
+
 }
